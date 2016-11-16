d0dbec417a6d535c89954666ff3e19082a74c2bb
Kamil Lasek <kamil.lasek@poczta.pl>
2016-11-16 21:41:15 +0100

    zmodyfikowany: app/build/outputs/apk/app-debug.apk 	zmodyfikowany: app/src/main/AndroidManifest.xml 	zmodyfikowany: app/src/main/java/com/example/kamilek/mytestapplicationqr/FullscreenActivity.java 	zmodyfikowany: build/generated/mockable-android-25.jar 	zmodyfikowany: build/intermediates/dex-cache/cache.xml

--- app/src/main/java/com/example/kamilek/mytestapplicationqr/FullscreenActivity.java
+++ app/src/main/java/com/example/kamilek/mytestapplicationqr/FullscreenActivity.java
@@ -1,11 +1,7 @@
 package com.example.kamilek.mytestapplicationqr;
 
-import android.annotation.SuppressLint;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
